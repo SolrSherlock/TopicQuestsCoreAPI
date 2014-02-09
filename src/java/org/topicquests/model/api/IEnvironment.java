5cf395a0531d88c8340e2fdf113b5303686f8955
Jack Park <jackpark@gmail.com>
2014-02-09 11:49:49 -0800

    First commit

---
+++ src/java/org/topicquests/model/api/IEnvironment.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.topicquests.model.api;
+
+import java.util.*;
+
+import org.topicquests.util.Tracer;
+
+/**
+ * @author park
+ *
+ */
+public interface IEnvironment {
+
+	IDataProvider getDataProvider();
+	
+	Map<String,Object> getProperties();
+	
+	String getStringProperty(String key);
+	
+	List<List<String>> getListProperty(String key);
+	
+	void shutDown();
+	
+	void logDebug(String msg);
+	
+	void logError(String msg, Exception e);
+	
+	void record(String msg);
+	
+	Tracer getTracer(String name);
+}
